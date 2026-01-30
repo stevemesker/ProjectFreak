7b9fe9fc3109fb58e4037ee7b206e85859a513df
Steve Mesker <steve@galliumstudios.com>
2026-01-30 08:39:29 -0600

    Class Data Setup
    Set up some basic data structures for item organization and player classes

---
+++ ProjectFreakBuild/Assets/Scripts/Common/UnitBaseClass.cs
@@ -0,0 +1,9 @@
+ namespace UnitBaseClass.Types
+{
+    public enum UnitBaseClass
+    {
+        Tech = 0,
+        Mutant = 1,
+        Barbarian = 2
+    }
+}
---
+++ ProjectFreakBuild/Assets/Scripts/Items/IngredientItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_NewItem", menuName = "ScriptableObjects/Items/Ingredient", order = 0)]
+
+public class IngredientItem : ItemSO
+{
+
+}
