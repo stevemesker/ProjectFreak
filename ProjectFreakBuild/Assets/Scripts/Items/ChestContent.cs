41e950c25bf6f7bcb6f9cc34bcd734436fcd1589
Steve Mesker <steve@galliumstudios.com>
2026-07-18 06:54:16 -0500

    timeline runner added

--- ProjectFreakBuild/Assets/Scripts/Items/ChestContent.cs
+++ ProjectFreakBuild/Assets/Scripts/Items/ChestContent.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class ChestContent : MonoBehaviour
 {
     public ItemSO content;
+    public TimelineRunner _TLine;
 
     public void openChest()
     {
         print("Boop");
+    }
+    public void test()
+    {
+        print("Test");
+    }
+    public void signalTest()
+    {
+        print("woof");
+        _TLine.ContinueTimeline();
     }
 }
